c94aa6d83599f3481dddf9309d40a32b06f36ace
MoritzSchueler96 <47536226+MoritzSchueler96@users.noreply.github.com>
2021-02-17 16:31:51 +0100

    added reward methods, added task loading, added plot func from Bohn, fixed make_env call

--- magpie/magpy/simple_train.py
+++ magpie/magpy/simple_train.py
@@ -14,6 +14,11 @@ from stable_baselines3.common.vec_env import (
     VecNormalize,
 )
 
+import random
+import datetime
+from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.monitor import Monitor
+
 import numpy as np
 import torch as th
 from gym import spaces
@@ -22,15 +27,12 @@ from gym import spaces
 from pyfly.pyfly import PyFly
 from pyfly.pid_controller import PIDController
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 
 from pyfly_fixed_wing_visualizer.pyfly_fixed_wing_visualizer import simrecorder
-
-# why different ones?
-# th.manual_seed(42)
-# np.random.seed(666)
 
 ########################################################################################################################################################
 # NOTE: fly to next point, no wind
@@ -59,14 +61,14 @@ from stable_baselines3.common.callbacks import BaseCallback
 
 
 # global variables
-render_interval = 600  # Time in seconds between rendering of training episodes
-test_interval = 50
+render_interval = 50000  # Time in seconds between rendering of training episodes
+test_interval = 500000
 last_test = 0
 last_render = time.time()
 checkpoint_save_interval = 300
 last_save = time.time()
 last_ep_info = None
-log_interval = 50
+log_interval = 5
 render_check = {"files": [], "time": time.time()}
 info_kw = [
     "success",
@@ -74,6 +76,9 @@ info_kw = [
     "end_error",
     "total_error",
     "success_time_frac",
+]
+info_kw = [
+    "rew",
 ]
 env = None
 model = None
@@ -118,13 +123,12 @@ class TensorboardCallback(BaseCallback):
                             else:
                                 info[k]["all"] = [ep_info[k]]
             if self.logger is not None:
-                if "success" in info:
-                    for measure in info_kw:
-                        for k, v in info[measure].items():
-                            self.logger.record(
-                                key="ep_info/{}_{}".format(measure, k),
-                                value=np.nanmean(v),
-                            )
+                for measure in info_kw:
+                    for k, v in info[measure].items():
+                        self.logger.record(
+                            key="ep_info/{}_{}".format(measure, k),
+                            value=np.nanmean(v),
+                        )
             elif (
                 self.locals["n_steps"] % log_interval == 0
                 and self.locals["n_steps"] != 0
@@ -144,6 +148,13 @@ class TensorboardCallback(BaseCallback):
             if now - last_render >= render_interval:
                 env.env_method(
                     "render",
+                    indices=0,
+                    mode="plot",
+                    show=False,
+                    close=True,
+                    save_path=os.path.join(
+                        model_folder, "render", str(self.num_timesteps)
+                    ),
                 )
                 last_render = time.time()
 
@@ -155,7 +166,7 @@ class TensorboardCallback(BaseCallback):
                     # writer=self.logger,
                 )
 
-            if now - render_check["time"] >= 30:
+            if now - render_check["time"] >= 300000:
                 for render_file in os.listdir(os.path.join(model_folder, "render")):
                     if render_file not in render_check["files"]:
                         render_check["files"].append(render_file)
@@ -220,6 +231,7 @@ class FixedWingAircraft_simple(gym.Env):
 
         self.rwd_vec = ["position_n", "position_e", "position_d", "Va"]
         self.rwd_weights = [1, 1, 1, 1]
+        self.rwd_method = ["euclid", "euclid", "euclid", "abs"]
         self.targets = [200, 0, -50, 22]
 
         self.rwd_delta = [5]
@@ -262,14 +274,38 @@ class FixedWingAircraft_simple(gym.Env):
         self._goal = self._goal_vel
         self.all_goals = [-1]
 
-    def sample_tasks(self, num_tasks):
-        np.random.seed(1337)
-        velocities = np.zeros(
-            num_tasks
-        )  # np.random.uniform(-0.5, 0.5, size=(num_tasks,))
-        tasks = [{"roll": velocity} for velocity in velocities]
+        # skip rendering
+        self.skip = False
+
+    def sample_tasks(num_tasks):
+        tasks = []
+        taskDir = os.path.join(startingDir, "tasks")
+        all_files = os.listdir(taskDir)
+        files = random.sample(all_files, num_tasks)
+        for f in files:
+            task = np.load(os.path.join(taskDir, f), allow_pickle=True)
+            tasks.append(task)
 
         return tasks
+
+    def sample_task(id):
+        if id == self.idx and self.cur_pos < len(self.tasks[id]):
+            self.cur_pos += 1
+        elif id == self.idx:
+            self.cur_pos = 0
+        else:
+            self.cur_pos = 0
+            self.idx = id
+        sample_target(id, self.cur_pos)
+
+    def sample_target(id, pos):
+        if pos == 0:
+            start = self.tasks[id][pos]
+            goal = self.tasks[id][pos + 1]
+        else:
+            start = self.tasks[id][pos - 1]
+            goal = self.tasks[id][pos]
+        # TODO: init Pyfly and set states
 
     def get_all_task_idx(pself):
         return range(len(self.tasks))
@@ -327,8 +363,9 @@ class FixedWingAircraft_simple(gym.Env):
 
         # action[2] = abs(action[2])
 
-        if self.simulator.cur_sim_step == 0:
-            self.rec = simrecorder(self.steps_max)
+        if not self.skip:
+            if self.simulator.cur_sim_step == 0:
+                self.rec = simrecorder(self.steps_max)
 
         control_input = list(action)
 
@@ -349,10 +386,11 @@ class FixedWingAircraft_simple(gym.Env):
             goal_achieved_on_step = False
 
             # save current state for visualization with pyfly fixed wing visualizer
-            if self.rec and self.steps_count % 50 == 0:
-                self.rec.savestate(
-                    self.simulator.state, self.simulator.cur_sim_step - 1
-                )
+            if not self.skip:
+                if self.rec:  # and self.steps_count % 50 == 0:
+                    self.rec.savestate(
+                        self.simulator.state, self.simulator.cur_sim_step - 1
+                    )
 
             # calculate reward  TODO: move down to get observation and appending?
             reward = self.get_reward(
@@ -369,7 +407,7 @@ class FixedWingAircraft_simple(gym.Env):
             reward = self.steps_count - self.steps_max
 
         obs = self.get_observation()
-        info = {}
+        info = {"rew": reward}
 
         return obs, reward, done, info
 
@@ -384,6 +422,9 @@ class FixedWingAircraft_simple(gym.Env):
         self.target["pitch"] = 0
         self.target["Va"] = 22
         self.target["position_n"] = 20
+
+    def set_skip(self, val):
+        self.skip = val
 
     def render(
         self,
@@ -403,10 +444,74 @@ class FixedWingAircraft_simple(gym.Env):
         :param save_path (str) if given, render is saved to this path.
         :return: (matplotlib Figure) if show is false in plot mode, the render figure is returned
         """
-        if epoch == None:
-            self.rec.plot(render="other")
-        else:
-            self.rec.plot(render="other", epoch=epoch)
+        if mode == "plt":
+            # get target values
+            targets = {
+                k: {"data": np.array(v)}
+                for k, v in self.history["target"].items()  # change with history
+            }
+
+            # create figure
+            self.viewer = {"fig": plt.figure(figsize=(9, 16))}
+
+            # set number of plots to create
+            subfig_count = 8
+
+            self.viewer["gs"] = matplotlib.gridspec.GridSpec(subfig_count, 1)
+
+            # plot actions
+            labels = [
+                a["name"] for a in self.cfg["action"]["states"]
+            ]  # change to fix names elev_left, elev_right, throttle
+            x, y = (
+                list(range(len(self.history["action"]))),
+                np.array(
+                    self.history["action"]
+                ),  # change to get values for elev und throttle
+            )
+            ax = plt.subplot(self.viewer["gs"][-2, 0], title="Actions")
+            for i in range(y.shape[1]):
+                ax.plot(x, y[:, i], label=labels[i])
+            ax.legend()
+
+            # plot rewards
+            x, y = (
+                list(range(len(self.history["reward"]))),
+                self.history["reward"],
+            )  # change to get reward values
+            ax = plt.subplot(self.viewer["gs"][-1, 0], title="Reward")
+            ax.plot(x, y)
+
+            # plot targets
+            self.simulator.render(close=close, targets=targets, viewer=self.viewer)
+
+            # save figures if specified
+            if save_path is not None:
+                if not os.path.isdir(os.path.dirname(save_path)):
+                    os.makedirs(os.path.dirname(save_path))
+                _, ext = os.path.splitext(save_path)
+                if ext != "":
+                    plt.savefig(save_path, bbox_inches="tight", format=ext[1:])
+                else:
+                    plt.savefig(save_path, bbox_inches="tight")
+
+            if show:
+                plt.show(block=block)
+                if close:
+                    plt.close(self.viewer["fig"])
+                    self.viewer = None
+            else:
+                if close:
+                    plt.close(self.viewer["fig"])
+                    self.viewer = None
+                else:
+                    return self.viewer["fig"]
+
+        elif mode == "other":
+            if epoch == None:
+                self.rec.plot(render="other")
+            else:
+                self.rec.plot(render="other", epoch=epoch)
 
     # TODO: change to work with array
     def apply_scaling(self):
@@ -432,10 +537,20 @@ class FixedWingAircraft_simple(gym.Env):
         # idea on how to calc reward without scaling
         rew = 0
         for r in enumerate(self.rwd_vec):
-            rew += self.rwd_weights[r[0]] * (
-                self.simulator.state[r[1]].value
-                - self.simulator.state[r[1]].history[-2]
-            )
+            if self.rwd_method[r[0]] == "abs":
+                rew += self.rwd_weights[r[0]] * (
+                    self.simulator.state[r[1]].value
+                    - self.simulator.state[r[1]].history[-2]
+                )
+            else:
+                rew += (
+                    self.rwd_weights[r[0]]
+                    * np.exp(
+                        self.simulator.state[r[1]].value
+                        - self.simulator.state[r[1]].history[-2]
+                    )
+                    ** 2
+                )
 
         # for idx, meas in enumerate(self.rwd_vec):
         #     if self.targets[idx] != 0:
@@ -471,11 +586,6 @@ class FixedWingAircraft_simple(gym.Env):
 ########################################################################################################################################################
 
 
-import datetime
-from stable_baselines3.common.utils import set_random_seed
-from stable_baselines3.common.monitor import Monitor
-
-
 def make_env(config_path, n_tasks, rank=0, seed=0, info_kw=None, sim_config_kw=None):
     """
     Utility function for multiprocessed env.
@@ -506,91 +616,109 @@ def save_model(model, save_folder):
     :param save_folder: (str) the folder to save model to.
     :return:
     """
-    model.save(os.path.join(save_folder, "model.pkl"))
     model.env.save(os.path.join(save_folder, "env.pkl"))
+    model.save(
+        os.path.join(save_folder, "model.pkl")
+    )  # FIX failes somehow only for mSAC
 
 
-modelname = os.path.join(
-    "Msac__", datetime.datetime.now().strftime("%H_%M%p__%B_%d_%Y")
-)
-model_folder = os.path.join(modelDir, modelname)
-os.makedirs(model_folder)
-os.makedirs(model_folder, "logs")
-file = os.path.join(model_folder, "logs/simpol.txt")
+if __name__ == "__main__":
+    modelname = "Msac__" + datetime.datetime.now().strftime("%H_%M%p__%B_%d_%Y")
+    model_folder = os.path.join(modelDir, modelname)
+    if not os.path.exists(model_folder):
+        os.makedirs(model_folder)
+        os.makedirs(os.path.join(model_folder, "logs"))
+    filename = "simpol.txt"
+    file = os.path.join(model_folder, "logs", filename)
 
-# env = make_env(config_path=os.path.join(configDir, "pyfly_config.json"), n_tasks=130)
-
-env = VecNormalize(
-    SubprocVecEnv(
-        make_env(config_path=os.path.join(configDir, "pyfly_config.json"), n_tasks=130)
-    )
-)
-
-meta_model = mSAC(
-    "MlpPolicy",
-    env,
-    n_traintasks=100,
-    n_evaltasks=30,
-    n_epochtasks=5,
-    verbose=1,
-    policy_kwargs=dict(
-        net_arch=[300, 300, 300], latent_dim=5, hidden_sizes=[200, 200, 200]
-    ),
-    tensorboard_log=os.path.join(modelDir, "tb"),
-)  # ,learning_rate=0.0006)
-
-meta_reward = []
-meta_std = []
-
-print("-Start-")
-n_eval = 30
-
-meta_model_mean_reward_before, meta_model_std_reward_before = evaluate_meta_policy(
-    meta_model, env, n_eval_episodes=n_eval, epoch=0
-)
-meta_reward.append(meta_model_mean_reward_before)
-meta_std.append(meta_model_std_reward_before)
-
-
-my_file = open(file, "w+")
-with open(file, "a") as myfile:
-    myfile.write("epoch:" + str(0) + "\n")
-    myfile.write("meta_reward = " + str(meta_reward) + "\n")
-    myfile.write("meta_std = " + str(meta_std) + "\n")
-    myfile.write(
-        "================================================================================================\n\n"
+    env = VecNormalize(
+        SubprocVecEnv(
+            [
+                make_env(
+                    config_path=os.path.join(configDir, "pyfly_config.json"),
+                    rank=n,
+                    seed=1337,
+                    info_kw=info_kw,
+                    n_tasks=130,
+                )
+                for n in range(1)
+            ]
+        )
     )
 
-print(
-    "##################################Start Learning##################################"
-)
-for i in range(50):
+    if False:
+        meta_model = mSAC(
+            "MlpPolicy",
+            env,
+            n_traintasks=100,
+            n_evaltasks=30,
+            n_epochtasks=5,
+            verbose=1,
+            policy_kwargs=dict(
+                net_arch=[300, 300, 300], latent_dim=5, hidden_sizes=[200, 200, 200]
+            ),
+            tensorboard_log=os.path.join(model_folder, "tb"),
+        )  # ,learning_rate=0.0006)
+    else:
+        meta_model = PPO(
+            "MlpPolicy",
+            env,
+            verbose=1,
+            tensorboard_log=os.path.join(model_folder, "tb"),
+        )
 
-    meta_model.learn(
-        total_timesteps=5 * 500,
-        callback=TensorboardCallback(),
-    )  # , eval_freq=100, n_eval_episodes=5, log_interval = 100)
-    meta_model_mean_reward, meta_model_std_reward = evaluate_meta_policy(
-        meta_model, env, n_eval_episodes=n_eval, epoch=i + 1
-    )
+    meta_reward = []
+    meta_std = []
 
-    meta_reward.append(meta_model_mean_reward)
-    meta_std.append(meta_model_std_reward)
+    print("-Start-")
+    n_eval = 30
 
-    print("epoch:", i)
-    print("meta_reward = ", meta_reward)
-    print("meta_std = ", meta_std)
+    # meta_model_mean_reward_before, meta_model_std_reward_before = evaluate_meta_policy(meta_model, env, n_eval_episodes=n_eval, epoch=0)
+    meta_model_mean_reward_before = 3
+    meta_model_mean_reward = 5
+    meta_model_std_reward_before = 4
+    meta_model_std_reward = 2
+    meta_reward.append(meta_model_mean_reward_before)
+    meta_std.append(meta_model_std_reward_before)
 
+    my_file = open(file, "w+")
     with open(file, "a") as myfile:
-        myfile.write("epoch:" + str(i + 1) + "\n")
+        myfile.write("epoch:" + str(0) + "\n")
         myfile.write("meta_reward = " + str(meta_reward) + "\n")
         myfile.write("meta_std = " + str(meta_std) + "\n")
         myfile.write(
             "================================================================================================\n\n"
         )
 
-save_model(meta_model, modelDir + modelname)
-# env.close()
+    print(
+        "##################################Start Learning##################################"
+    )
+    for i in range(50):
+
+        meta_model.learn(
+            total_timesteps=5 * 500,
+            callback=TensorboardCallback(),  # FIX creates new tb file each time called
+        )  # , eval_freq=100, n_eval_episodes=5, log_interval = 100)
+
+        # meta_model_mean_reward, meta_model_std_reward = evaluate_meta_policy(    meta_model, env, n_eval_episodes=n_eval, epoch=i + 1)
+
+        meta_reward.append(meta_model_mean_reward)
+        meta_std.append(meta_model_std_reward)
+
+        print("epoch:", i)
+        print("meta_reward = ", meta_reward)
+        print("meta_std = ", meta_std)
+
+        with open(file, "a") as myfile:
+            myfile.write("epoch:" + str(i + 1) + "\n")
+            myfile.write("meta_reward = " + str(meta_reward) + "\n")
+            myfile.write("meta_std = " + str(meta_std) + "\n")
+            myfile.write(
+                "================================================================================================\n\n"
+            )
+
+    save_model(meta_model, modelDir + modelname)
+    # env.close()
 
 
 ########################################################################################################################################################
